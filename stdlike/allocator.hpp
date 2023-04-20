14d92bba6fbcbe73f043fe64ff158e5651130cb0
Stanislav Goriainov <goriainov@ispras.ru>
2023-04-20 14:51:02 +0300

    Add stdlike llibrary to the project.

--- stdlike/allocator.hpp
+++ stdlike/allocator.hpp
@@ -1,19 +1,22 @@
-#include <cstddef>
+#include <limits>
+#include <memory>
 #include <new>
 #include <numeric>
+
+#include "utility.hpp"
 
 namespace stdlike {
 
 template <typename Type>
 class Allocator {
 public:
+    using size_type  = size_t;
     using value_type = Type;
-    using size_type = size_t;
     using difference_type = ptrdiff_t;
 
-    using pointer = Type*;
-    using const_pointer = const Type*;
+    using pointer   = Type*;
     using reference = Type&;
+    using const_pointer   = const Type*;
     using const_reference = const Type&;
 
     Allocator() {
@@ -22,15 +25,15 @@ public:
     Allocator([[maybe_unused]] const Allocator& other) {
     }
 
-    template <typename U>
-    Allocator([[maybe_unused]] const Allocator<U>& other) {
+    template <typename OtherType>
+    Allocator([[maybe_unused]] const Allocator<OtherType>& other) {
     }
 
     ~Allocator() {
     }
 
     pointer Address(reference value) {
-        pointer addr = Address(value);
+        return std::__addressof(value);
     }
 
     const_pointer Address(reference value) const {
@@ -45,7 +48,7 @@ public:
 
     void Deallocate(pointer ptr, size_type elems_n) {
         std::align_val_t align = std::align_val_t(alignof(value_type));
-        ::operator delete(ptr, elems_n * sizeof(value_type), align);
+        operator delete (ptr, elems_n * sizeof(value_type), align);
     }
 
     size_type MaxSize() const {
@@ -53,12 +56,12 @@ public:
     }
 
     void Construct(pointer ptr, const_reference value) {
-        ::new (ptr) Type(value);
+        new (ptr) Type(value);
     }
 
     template <class Other, class... Args>
     void Construct(Other* ptr, Args&&... args) {
-        ::new (static_cast<void*>(ptr)) Other(std::forward<Args>(args)...);
+        new (static_cast<void*>(ptr)) Other(stdlike::Forward<Args>(args)...);
     }
 
     void Destroy(pointer ptr) {
@@ -70,7 +73,7 @@ public:
         ptr->~Other();
     }
 
-    /* Compatability */
+    // Compatability
 
     pointer address(reference value) {
         return Address(value);
@@ -80,8 +83,8 @@ public:
         return address(value);
     }
 
-    [[nodiscard]] constexpr pointer allocate(size_type elems_n, [[maybe_unused]] const void* hint = nullptr) {
-        return Allocate(elems_n);
+    [[nodiscard]] constexpr pointer allocate(size_type elems_n, const void* hint = nullptr) {
+        return Allocate(elems_n, hint);
     }
 
     void deallocate(pointer ptr, size_type elems_n) {
@@ -98,7 +101,7 @@ public:
 
     template <class Other, class... Args>
     void construct(Other* ptr, Args&&... args) {
-        Construct(ptr, std::forward<Args>(args)...);
+        Construct(ptr, stdlike::Forward<Args>(args)...);
     }
 
     void destroy(pointer ptr) {
@@ -109,10 +112,6 @@ public:
     void destroy(Other* ptr) {
         Destroy(ptr);
     }
-};
-
-template <>
-class Allocator<bool> {
 };
 
 }  // namespace stdlike
---
+++ tests/stdlike/vector.cpp
@@ -0,0 +1,84 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+
+#include "vector.hpp"
+
+class TestClass {
+public:
+    TestClass(int val) {
+        m_val = val;
+    }
+
+    int val() {
+        return m_val;
+    }
+
+private:
+    int m_val = 0;
+};
+
+int main() {
+    stdlike::Vector<stdlike::Vector<int>> test1(2, stdlike::Vector<int>(3, 1));
+    std::cout << "Test #1: (create a vector of vectros of ints)" << std::endl;
+    std::cout << test1 << std::endl << std::endl;
+    
+    test1.Insert(test1.Begin() + 1, stdlike::Vector<int>(2, 2));
+    std::cout << "Test #2: (insert into a vector of vectros of ints)" << std::endl;
+    std::cout << test1 << std::endl << std::endl;
+
+    test1.Erase(test1.Begin() + 1);
+    std::cout << "Test #3: (erase from a vector of vectros of ints)" << std::endl;
+    std::cout << test1 << std::endl << std::endl;
+
+    stdlike::Vector<stdlike::Vector<int>> test2 = test1;
+    std::cout << "Test #4: (copy a vector of vectros of ints)" << std::endl;
+    std::cout << test2 << std::endl << std::endl;
+
+    test1.PushBack(stdlike::Vector<int>(2, 2));
+    std::cout << "Test #5: (push_back into a vector of vectros of ints)" << std::endl;
+    std::cout << test1 << std::endl << std::endl;
+
+    test2.Swap(test1);
+    std::cout << "Test #6: (swap two vectors of vectros of ints)" << std::endl;
+    std::cout << test1 << std::endl << test2 << std::endl << std::endl;
+    test1.Swap(test2);
+
+    test1.PopBack();
+    std::cout << "Test #7: (pop_back from a vector of vectros of ints)" << std::endl;
+    std::cout << test1 << std::endl << std::endl;
+
+    test1.Clear();
+    std::cout << "Test #8: (clear a vector of vectros of ints)" << std::endl;
+    std::cout << test1.Size() << " " << test1.Capacity() << std::endl << std::endl;
+
+    test1.ShrinkToFit();
+    std::cout << "Test #9: (shrink_to_fit a vector of vectros of ints)" << std::endl;
+    std::cout << test1.Size() << " " << test1.Capacity() << std::endl << std::endl;
+
+    stdlike::Vector<TestClass> test3(1, TestClass(2));
+    std::cout << "Test #10: (create a vector of TestClass objects)" << std::endl;
+    std::cout << test3.At(0).val() << std::endl;
+
+    stdlike::Vector<bool> bvector_test(10, true);
+    bvector_test.PushBack(false);
+    auto found = std::find(bvector_test.begin(), bvector_test.end(), false);
+
+    if (found != bvector_test.end()) {
+        std::cout << "Found" << std::endl;
+    } else {
+        std::cout << "Not found" << std::endl;
+    }
+
+    stdlike::Vector<int> vector_test(10, 9);
+    vector_test.PushBack(10);
+    auto found2 = std::find(vector_test.begin(), vector_test.end(), 10);
+
+    if (found2 != vector_test.end()) {
+        std::cout << "Found" << std::endl;
+    } else {
+        std::cout << "Not found" << std::endl;
+    }
+
+    return 0;
+}
