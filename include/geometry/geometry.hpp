3f2216ef15e1eb9e6cd8faf5c4eaba6d48730439
Gekata <y.zvonkov@yandex.ru>
2023-04-26 18:29:10 +0300

    fix random conflict

--- include/geometry/geometry.hpp
+++ include/geometry/geometry.hpp
@@ -8,32 +8,32 @@
 #include <geometry/sphere.hpp>
 #include <geometry/intersects.hpp>
 #include <geometry/operations.hpp>
-#include <random>
+// #include <random>
 
 namespace geometry {
 
-inline double RandomDouble() {
-    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
-    static std::mt19937 generator;
-    return distribution(generator);
-}
+// inline double RandomDouble() {
+//     static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+//     static std::mt19937 generator;
+//     return distribution(generator);
+// }
 
-inline double RandomDouble(double min, double max) {
-    return min + (max - min) * RandomDouble();
-}
+// inline double RandomDouble(double min, double max) {
+//     return min + (max - min) * RandomDouble();
+// }
 
-inline Vector3f RandomInSphere() {
-    Vector3f v{};
-    do {
-        v = {
-            RandomDouble(-1, 1),
-            RandomDouble(-1, 1),
-            RandomDouble(-1, 1),
-        };
+// inline Vector3f RandomInSphere() {
+//     Vector3f v{};
+//     do {
+//         v = {
+//             RandomDouble(-1, 1),
+//             RandomDouble(-1, 1),
+//             RandomDouble(-1, 1),
+//         };
 
-    } while (v.Len2() > 1);
-    return Normalize(v);
-}
+//     } while (v.Len2() > 1);
+//     return Normalize(v);
+// }
 }  // namespace Geometry
 
 #endif /* GEOMETRY_GEOMETRY_HPP */
