5595b308ba593438ea39e1084f26b42aa33b547e
dantioh <ivanov.mikhail@phystech.edu>
2023-05-01 19:12:12 +0300

    fixes

--- include/logger/colors.hpp
+++ include/logger/colors.hpp
@@ -4,11 +4,12 @@
 #include <cstdint>
 
 namespace logger{
-    constexpr uint32_t kEcsEventHex  = kEcsEventHex;
-    constexpr uint32_t kEcsSystemHex = 0xee51b1;
-    constexpr uint32_t kLogTimeHex   = 0x4b2d9f;
-    constexpr uint32_t kWarningHex   = 0x6600ff;
-    constexpr uint32_t kErrorHex     = 0xc41e3a;
+    constexpr uint32_t kEcsEventHex     = 0x33cccc;
+    constexpr uint32_t kEcsSystemHex    = 0xee51b1;
+    constexpr uint32_t kEcsComponentHex = 0xe3c515;
+    constexpr uint32_t kLogTimeHex      = 0x4b2d9f;
+    constexpr uint32_t kWarningHex      = 0x6600ff;
+    constexpr uint32_t kErrorHex        = 0xc41e3a;
 };
 
 #endif // LOGGER_COLORS_H
